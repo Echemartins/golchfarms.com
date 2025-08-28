2855d890b67b2943a3d089e06c566fc30d57ed6a
echemartins <echemartins47@gmail.com>
2025-08-28 22:35:08 +0100

    corrected build error

--- lib/db.ts
+++ lib/db.ts
@@ -1,25 +1,4 @@
 // lib/db.ts
-// import mongoose from 'mongoose';
-
-// const MONGODB_URI = process.env.MONGODB_URI as string;
-
-// if (!MONGODB_URI) {
-//   throw new Error('Please define the MONGODB_URI environment variable in .env.local');
-// }
-
-// let cached = (global as any).mongoose as { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
-// if (!cached) cached = (global as any).mongoose = { conn: null, promise: null };
-
-// export async function dbConnect() {
-//   if (cached.conn) return cached.conn;
-//   if (!cached.promise) {
-//     cached.promise = mongoose.connect(MONGODB_URI).then((m) => m);
-//   }
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// }
-
-// lib/db.ts
 import mongoose from 'mongoose';
 
 
@@ -34,7 +13,6 @@ promise: Promise<typeof mongoose> | null;
 
 
 declare global {
-// eslint-disable-next-line no-var
 var _mongoose: MongooseCache | undefined;
 }
 
@@ -46,7 +24,7 @@ if (!global._mongoose) global._mongoose = globalCache;
 export async function dbConnect(): Promise<typeof mongoose> {
 if (globalCache.conn) return globalCache.conn;
 if (!globalCache.promise) {
-globalCache.promise = mongoose.connect(MONGODB_URI, { dbName: undefined }).then((m) => m);
+globalCache.promise = mongoose.connect(MONGODB_URI).then((m) => m);
 }
 globalCache.conn = await globalCache.promise;
 return globalCache.conn;
